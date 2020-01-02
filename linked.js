c905356497ab35f1d006500702ae18a1a1bfd056
Dan <55209299+eco-d@users.noreply.github.com>
2020-01-02 12:23:22 -0500

    HardCodeTime Update
    Utilize Meta tag, integrate with cool fun cool gSheets data. Stupify and Smartify code. 
    
    
    Shoutout Elliot for gSheets

--- linked.js
+++ linked.js
@@ -1,12 +1,4 @@
 /*
-input.html (game_setup) ids:
-  Name_of_restarter
-  Num_of_accidents
-  Num_of_speeding
-  starting_pot
-  pot_increase
-  pot_max
-  reset_button
 
 index.html (gameboard) ids:
   jackpot
@@ -17,122 +9,65 @@ index.html (gameboard) ids:
   shame
   n1-n75
 
+data comes in as:
+  start date
+  current jackpot
+  accidents
+  speeding
+  today's number
+  restarted by
+
 */
 
-//global variables
-var in_ids = ["Name_of_restarter","Num_of_accidents","Num_of_speeding","starting_pot","pot_increase","pot_max"];
-var dis_ids = ["shame","accidents","speeding","jackpot"];
-var holders = [];
-var called_nums = [];
+async function getGsheetData(sheetId, tabId) {
+    // Steps to set this up
+    // 1. publish all the sheets you want to get data from in gSheets
+    // 2. allow links in the sharing properties
+    // 3. retrieve the sheetId from the url
+    //      - looks like this: '1qkR5ALHeEe8TKRBiiAiw_mFmhJ4HsNDPvgulL0ccGhM'
+    // 4. determine the tabId. This should be an integer starting at 1
+    //    in the order the tabs were created
 
+    var nums = [];
+    var url = `https://spreadsheets.google.com/feeds/cells/${sheetId}/${tabId}/public/values?alt=json`;
+    var dt = [];
+    await $.getJSON(url, function (data) {
 
-//functions
-function startDate(){
-  var a = [1,2,3,4,5,6,7,8,9,10,11,12];
-  var d = new Date();
+        var entry = data.feed.entry;
 
-  var hold = a[d.getMonth()] + "/" + d.getDate() + "/" + d.getFullYear();
-  $("#startdate").html(hold);
+        for (var i = 0; i < entry.length; i++) {
+            dt.push(entry[i].content.$t);
+        }
+
+    });
+    return dt;
+
 }
 
-function pullOver(ins, h){
-  for(var i = 0; i < ins.length; i++){
-    var hold = "#" + ins[i];
-    h.push($(hold).val());
-  }
-  return(h);
-}
+// Specifics for Safety Bingo
+bingoSheet = '1qkR5ALHeEe8TKRBiiAiw_mFmhJ4HsNDPvgulL0ccGhM';
 
-function pushInto(d, h){
-  $("#"+d[0]).html(h[0]);
-  $("#"+d[1]).html(h[1] + " Days");
-  $("#"+d[2]).html(h[2] + " Days");
-  $("#"+d[3]).html("$" + h[3]);
-}
+getGsheetData(bingoSheet, 2).then((setup)  => {
+    console.log(setup);
+    $('#startdate').html(setup[0]);
+    $('#jackpot').html(setup[1]);
+    $('#accidents').html(setup[2]);
+    $('#speeding').html(setup[3]);
+    $('#today_num').html(setup[4]);
+    $('#shame').html(setup[5]);
+});
 
-function clearCalled(n){
-  var hold = "";
-  for(var i = 1; i< n.length; i++){
-    hold = "#n" + i;
-    $(hold).attr('class', 'bb-txt');
-  }
-}
-
-function newNum(n){
-
-  var bad = true;
-  var nu = 0;
-
-  while(bad){
-
-    bad = false;
-    nu = Math.round((Math.random()*75)+1);
-
-    for(var i = 0; i < n.length; i++){
-      if(nums[i] == nu){
-        bad = true;
-      }
+getGsheetData(bingoSheet,3).then((numbers) => {
+    console.log(numbers);
+    for(var i = 1; i < 76; i++){
+      hold = "#n" + i;
+      console.log(hold);
+      $(hold).attr('class', 'bb-txt');
     }
-  }
-  n.push(nu);
-  $("#today_num").html(nu);
-  hold = "#n" + nu;
-  $(hold).attr('class', 'bb-txt picked');
 
-}
-
-
-function update(h){
-  var addr = h[4];
-  var max = h[5];
-  var holdS = parseInt($("#speeding").html());
-  var holdA = parseInt($("#accidents").html());
-  var holdM = parseInt($("#jackpot").html().slice(1));
-
-  holdS = holdS + 1;
-  holdA = holdA + 1;
-  if((addr+holdM) <= max){
-    holdM = holdM + addr;
-  }
-
-  $("#speeding").html(holdS + " Days");
-  $("#accidents").html(holdA + " Days");
-  $("#jackpot").html("$" + holdM);
-
-
-}
-
-
-function play(holder, n) {
-  var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var d = today.getDay();
-
-  newNum(n);
-  update(holder);
-
-  // if(d != 0 && d!= 6){
-  //   if(h == 6){
-  //     if(m == 50 || m == 51 || m == 52){
-  //       newNum(n);
-  //       update(holder);
-  //     }
-  //   }
-  // }
-  t = setTimeout(function() {
-    play(holder, n);
-  }, 15000); //180000
-}
-
-
-//function calls
-$("#resetter").on('click', function() {
-  pullOver(in_ids, holders);
-  pushInto(dis_ids, holders);
-  clearCalled(called_nums);
-  newNum(called_nums);
-  startDate();
-  $("#game_setup").fadeOut();
-  play(holder, called_nums);
+    for(var j = 0; j < numbers.length; j++){
+      hold = "#n" + numbers[j];
+      console.log("part 2: " + hold);
+      $(hold).attr('class', 'bb-txt picked');
+    }
 });
